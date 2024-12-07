89a3288b6901a0eeca214e8b7bfb3fdbf65d2a23
vkanada6417 <165505231+vkanada6417@users.noreply.github.com>
2024-12-07 23:14:36 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -6,7 +6,11 @@ class Pokemon:
     
     def __init__(self, pokemon_trainer):
         self.pokemon_trainer = pokemon_trainer
-        self.pokemon_number = randint(1, 1000)
+        self.pokemon_number = self.random_pokemon_number()
+        self.level = 1
+        self.experience = 0
+        self.hunger = 50  # Начальный уровень сытости (0–100)
+        self.is_rare = False
         
         # Получаем данные из API
         data = self.fetch_data()
@@ -27,6 +31,11 @@ class Pokemon:
         
         Pokemon.pokemons[pokemon_trainer] = self
 
+    def random_pokemon_number(self):
+        # С шансом 5% генерируем редкого покемона
+        self.is_rare = randint(1, 100) <= 5
+        return randint(1, 1000)
+
     def fetch_data(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
@@ -35,31 +44,46 @@ class Pokemon:
         return None
 
     def info(self):
+        rarity = "Редкий" if self.is_rare else "Обычный"
         types = ", ".join(self.types) if self.types else "Unknown"
         abilities = ", ".join(self.abilities) if self.abilities else "None"
         return (f"Имя: {self.name}\n"
+                f"Редкость: {rarity}\n"
+                f"Уровень: {self.level}\n"
+                f"Опыт: {self.experience}\n"
+                f"Сытость: {self.hunger}\n"
                 f"Рост: {self.height / 10} м\n"
                 f"Вес: {self.weight / 10} кг\n"
                 f"Типы: {types}\n"
                 f"Способности: {abilities}")
-    
+
     def show_img(self):
         return self.img
 
-    # Методы для изменения свойств
-    def set_name(self, new_name):
-        self.name = new_name
+    # Методы для взаимодействия
+    def feed(self):
+        if self.hunger < 100:
+            self.hunger = min(self.hunger + 20, 100)
+            return "Покемон накормлен! Сытость увеличилась."
+        return "Покемон уже сыт!"
 
-    def set_height(self, new_height):
-        self.height = new_height
+    def gain_experience(self, amount):
+        self.experience += amount
+        if self.experience >= self.level * 10:
+            self.level_up()
 
-    def set_weight(self, new_weight):
-        self.weight = new_weight
+    def level_up(self):
+        self.level += 1
+        self.experience = 0
+        return f"Поздравляем! {self.name} достиг уровня {self.level}!"
 
-    def add_ability(self, ability):
-        if ability not in self.abilities:
-            self.abilities.append(ability)
+    def check_hunger(self):
+        if self.hunger > 0:
+            self.hunger -= 5  # Уменьшаем сытость со временем
+        else:
+            return f"{self.name} голоден! Скоро он перестанет расти."
 
-    def add_type(self, new_type):
-        if new_type not in self.types:
-            self.types.append(new_type)
+    def add_achievement(self):
+        return f"Поздравляем! Вы получили достижение за владение редким покемоном: {self.name}!"
+
+
