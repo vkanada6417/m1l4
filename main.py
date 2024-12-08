4ebbbdb7645bd3e4032efdda8d28c590a8b091ba
vkanada6417 <165505231+vkanada6417@users.noreply.github.com>
2024-12-08 11:14:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import telebot 
 from config import token
-from logic import Pokemon
+from random import randint
+from logic import Pokemon, Wizard, Fighter
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['go'])
-def go(message):
+def start(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+        chance = randint(1,5)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = Fighter(message.from_user.username)
+        else:
+            pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
-        
-        if pokemon.is_rare:
-            bot.send_message(message.chat.id, pokemon.add_achievement())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
@@ -25,7 +31,7 @@ def rename(message):
 
     if message.from_user.username in Pokemon.pokemons:
         new_name = args[1]
-        Pokemon.pokemons[message.from_user.username].set_name(new_name)
+        Pokemon.pokemons[message.from_user.username].name = new_name
         bot.send_message(message.chat.id, f"Имя покемона изменено на {new_name}")
     else:
         bot.reply_to(message, "Сначала создайте покемона с помощью команды /go")
@@ -35,6 +41,8 @@ def info(message):
     if message.from_user.username in Pokemon.pokemons:
         pokemon = Pokemon.pokemons[message.from_user.username]
         bot.send_message(message.chat.id, pokemon.info())
+        bot.send_message(message.chat.id, f"Здоровье: {pokemon.hp}")
+        bot.send_message(message.chat.id, f"Сила: {pokemon.power}")
     else:
         bot.reply_to(message, "Сначала создайте покемона с помощью команды /go")
 
@@ -47,16 +55,42 @@ def feed(message):
     else:
         bot.reply_to(message, "Сначала создайте покемона с помощью команды /go")
 
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+        bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
+
 @bot.message_handler(commands=['help'])
 def help(message):
-    help_text = (
-        "Доступные команды:\n"
-        "/go - Создать нового покемона.\n"
-        "/info - Показать информацию о вашем покемоне.\n"
-        "/rename <новое_имя> - Изменить имя вашего покемона.\n"
-        "/feed - Покормить покемона.\n"
-        "/help - Показать список команд и их описание."
-    )
-    bot.send_message(message.chat.id, help_text)
+    try:
+        help_text = (
+            "Доступные команды:\n"
+            "/go - Создать нового покемона.\n"
+            "/info - Показать информацию о вашем покемоне.\n"
+            "/rename <новое_имя> - Изменить имя вашего покемона.\n"
+            "/feed - Покормить покемона.\n"
+            "/heal - Восстановить здоровье покемона.\n"
+            "/help - Показать список команд и их описание."
+        )
+        bot.send_message(message.chat.id, help_text)
+    except Exception as e:
+        bot.reply_to(message, "Ошибка при выполнении команды /help")
+
+@bot.message_handler(commands=['heal'])
+def heal_pok(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        pok.heal(10)
+        bot.send_message(message.chat.id, f"Здоровье покемона восстановлено на 10 единиц. Текущее здоровье: {pok.hp}")
+    else:
+        bot.send_message(message.chat.id, "Сначала создайте покемона")
 
 bot.infinity_polling(none_stop=True)
